0547a846287c5c22fff4b565a9e4e6a0e78e9ece
Shelby <nanadanso533@gmail.com>
2026-05-31 02:43:55 +0000

    changed

--- initials.py
+++ initials.py
@@ -1,7 +1,26 @@
-
+# File: initials.py
+# Description: Print out my initials
+# Assignment Number: 1
+#
+# Name: Nana Danso Asiam
+# STUDENT ID: 2425402956
+# Email: 2425402956@gctu.live.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Nana Danso Asiam, this programming assignment is my own work
+# and I have not provided this code to any other student.
 def main():
-  pass #code goes here
-
-
-
-main()
+    # This function prints the initials for NDA.
+    print()
+    print("...NDA")
+    print()
+    print("...NNNN......NN........DDDDDDDDD..............AAAAAA........")
+    print("...NNNNN.....NN........DDDDDDDDDD............AAAAAAAA.......")
+    print("...NN.NN.....NN........DD........DD.........AAA....AAA......")
+    print("...NN..NN....NN........DD........DD........AA........AA.....")
+    print("...NN...NN...NN........DD........DD........AA........AA.....")
+    print("...NN....NN..NN........DD........DD........AAAAAAAAAAAA.....")
+    print("...NN.....NN.NN........DD........DD........AA........AA.....")
+    print("...NN......NNNN...**...DDDDDDDDDD.....**...AA........AA...**")
+    print("...NN.......NNN...**...DDDDDDDDD......**...AA........AA...**")
+    print()
